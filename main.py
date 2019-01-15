83d30bafcd47b68aa1c17738887b37856af6e7cd
Arjun Bemarkar <arjunbemarkar@Arjuns-MacBook-Air.local>
2019-01-14 18:04:53 -0800

    added 404 handling

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ from passlib.hash import sha256_crypt
 from wtforms import Form, TextField, PasswordField, validators
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
+from alert import AlertType as at
 import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////Users/arjunbemarkar/Python/Flask/CTF-Client/databases/users.db"
 db = SQLAlchemy(app)
 app.secret_key = os.urandom(24)
-
+print(at.red.value)
 class User(db.Model):
 	__tablename__ = "users"
 	id = db.Column(db.Integer, primary_key=True)
@@ -39,10 +40,11 @@ def login():
 			hashed_password = user.password
 			attempted_password = str(request.form['password'])
 			if check_password_hash(hashed_password, attempted_password):
+				session['username'] = user.username
 				return redirect(url_for("home"))
-			flash(u"Incorrect Password", 'danger')
+			flash(u"Incorrect Password", at.red)
 			return render_template("SigninOrSignup.html", type="Log In", session=session)
-		flash(u"User Does Not Exist", 'danger')
+		flash(u"User Does Not Exist", at.red.value)
 	return render_template("SigninOrSignup.html", type="Log In", session=session)
 
 @app.route("/signup/", methods=["GET", "POST"])
@@ -54,13 +56,13 @@ def signup():
 		password = generate_password_hash(str(request.form['password']), method='sha256')
 		x = User.query.filter_by(username=username).first()
 		if x:
-			flash(u"This username has already been taken", 'danger')
+			flash(u"This username has already been taken", at.red.value)
 		else:
 			new_user = User(username=username, password=password)
 			db.session.add(new_user)
 			db.session.commit()
 			session['username'] = new_user.username
-			flash(u"new user has been created!", 'success')
+			flash(u"new user has been created!", at.green.value)
 	return render_template("SigninOrSignup.html", type="Sign Up", session=session)
 
 @app.route("/logout/")
@@ -73,7 +75,12 @@ def test():
 	if 'username' in session:
 		return "Yey"
 	else:
-		return "<h1>you dont have access to this page</h1>"
+		return "<h1>you dont have access to this page, {}</h1>".format(session)
+		print session
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
 
 if __name__ == "__main__":
 	app.run(threaded=True)
